e76f9b01baf08f42dd333b279a66cec511279eda
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-05 16:26:48 -0400

    Switched to psql db; added an import sys and dj_database_url

--- hackernewsclone/hackernewsclone/settings.py
+++ hackernewsclone/hackernewsclone/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import sys
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'hackernewsclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
     }
 }
 
